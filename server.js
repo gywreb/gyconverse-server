713d3ec2ad6b3368ad3678231857324421db3ac4
gywreb <longnguyenhoang91@gmail.com>
2021-08-08 13:03:21 +0700

    update swagger docs

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require("colors");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const swaggerDoc = require("./swagger.json");
+const swaggerUI = require("swagger-ui-express");
 const ConnectMongo = require("./database/dbConnect");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
@@ -22,6 +24,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.use("/gyconverse/api/auth", auth);
 app.use("/gyconverse/api/user", user);
